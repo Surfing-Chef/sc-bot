f79647ebeeec3e089f1b74682f44ec13b8ce52be
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-20 10:29:19 -0700

    Edited code.  Refined function adjustment to certain links before outputting.

--- index.php
+++ index.php
@@ -14,6 +14,18 @@ function follow_links($url){
     $l =  $link->getAttribute("href");
     if (substr($l, 0, 1) == '/' && substr($l, 0, 2) != '//') {
       $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . $l;
+    } else if (substr($l, 0, 2) == '//') {
+      $l = parse_url($url)['scheme'] . ":" . $l;
+    } else if (substr($l, 0, 2) == './') {
+      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . dirname(parse_url($url)['path']) . substr($l, 1);
+    } else if (substr($l, 0, 1) == '#'){
+      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . parse_url($url)['path'] . $l;
+    } else if (substr($l, 0, 3) == '../'){
+      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . '/' . $l;
+    } else if (substr($l, 0, 11) == 'javascript:'){
+      continue;
+    } else if (substr($l, 0, 5) != 'https' && substr($l, 0, 4) != "http") {
+      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . '/' . $l;
     }
 
     echo $l . "\n";
