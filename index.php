c7feecb30ed50fbb0b2bd10e15adfbdb3e157d31
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-20 11:47:32 -0700

    Edited code.  Used parsed links to pull in requested data from url.

--- index.php
+++ index.php
@@ -2,16 +2,66 @@
 
 $start = "http://localhost/sc-bot/test.html";
 
-function follow_links($url){
+$already_crawled = array();
+
+function get_details($url){
+
+  $options = array(
+    'http'=>array(
+      'method'=>"GET",
+      'headers'=>"User-Agent: sc-Bot/0.1\n"
+    )
+  );
+
+  $context = stream_context_create($options);
 
   $doc = new DOMDocument();
-  $doc->loadHTML(file_get_contents($url));
+  @$doc->loadHTML(@file_get_contents($url, false, $context));
 
-  $linklist = $doc->getElementsByTagName('a');
+  $title = @$doc->getElementsByTagName("title");
+  $title = @$title->item(0)->nodeValue;
+
+  $description = "";
+  $keywords = "";
+  $metas = $doc->getElementsByTagName("meta");
+  for ( $i = 0; $i < $metas->length; $i++ ){
+    $meta = $metas->item($i);
+
+    if ($meta->getAttribute("name") == strtolower("description")){
+      $description = $meta->getAttribute("content");
+    }
+    if ($meta->getAttribute("name") == strtolower("keywords")){
+      $keywords = $meta->getAttribute("content");
+    }
+
+  }
+
+  echo $keywords."\n";
+
+}
+
+function follow_links($url){
+
+  global $already_crawled;
+
+  $options = array(
+    'http'=>array(
+      'method'=>"GET",
+      'headers'=>"User-Agent: sc-Bot/0.1\n"
+    )
+  );
+
+  $context = stream_context_create($options);
+
+  $doc = new DOMDocument();
+  @$doc->loadHTML(@file_get_contents($url, false, $context));
+
+  $linklist = @$doc->getElementsByTagName('a');
 
   foreach ($linklist as $link){
 
     $l =  $link->getAttribute("href");
+
     if (substr($l, 0, 1) == '/' && substr($l, 0, 2) != '//') {
       $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . $l;
     } else if (substr($l, 0, 2) == '//') {
@@ -28,10 +78,18 @@ function follow_links($url){
       $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . '/' . $l;
     }
 
-    echo $l . "\n";
+    if(!in_array($l, $already_crawled)){
+
+      $already_crawled[] = $l;
+      echo get_details($l);
+      // echo $l . "\n";
+
+    }
 
   }
 
 }
 
 follow_links($start);
+
+print_r($already_crawled);
