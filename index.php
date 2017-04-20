a9bfce554270058b608e7a671f55eb73c66ba804
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-20 13:32:55 -0700

    Started customizing for single site data parse.

--- index.php
+++ index.php
@@ -1,109 +1,19 @@
-<?php
-
-$start = "http://localhost/sc-bot/test.html";
-
-$already_crawled = array();
-$crawling = array();
-
-function get_details($url){
-
-  $options = array(
-    'http'=>array(
-      'method'=>"GET",
-      'headers'=>"User-Agent: sc-Bot/0.1\n"
-    )
-  );
-
-  $context = stream_context_create($options);
-
-  $doc = new DOMDocument();
-  @$doc->loadHTML(@file_get_contents($url, false, $context));
-
-  $title = @$doc->getElementsByTagName("title");
-  $title = @$title->item(0)->nodeValue;
-
-  $description = "";
-  $keywords = "";
-  $metas = $doc->getElementsByTagName("meta");
-  for ( $i = 0; $i < $metas->length; $i++ ){
-    $meta = $metas->item($i);
-
-    if ($meta->getAttribute("name") == strtolower("description")){
-      $description = $meta->getAttribute("content");
-    }
-    if ($meta->getAttribute("name") == strtolower("keywords")){
-      $keywords = $meta->getAttribute("content");
-    }
-
-  }
-
-  return '{
-    "Title":"'.$title.'",
-    "Description":"'.str_replace("n","", $description).'",
-    "Keywords":"'.str_replace("n","", $keywords).'",
-    "URL":"'.str_replace("n","", $url).',"
-  }';
-
-}
-
-function follow_links($url){
-
-  global $already_crawled;
-  global $crawling;
-
-  $options = array(
-    'http'=>array(
-      'method'=>"GET",
-      'headers'=>"User-Agent: sc-Bot/0.1\n"
-    )
-  );
-
-  $context = stream_context_create($options);
-
-  $doc = new DOMDocument();
-  @$doc->loadHTML(@file_get_contents($url, false, $context));
-
-  $linklist = @$doc->getElementsByTagName('a');
-
-  foreach ($linklist as $link){
-
-    $l =  $link->getAttribute("href");
-
-    if (substr($l, 0, 1) == '/' && substr($l, 0, 2) != '//') {
-      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . $l;
-    } else if (substr($l, 0, 2) == '//') {
-      $l = parse_url($url)['scheme'] . ":" . $l;
-    } else if (substr($l, 0, 2) == './') {
-      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . dirname(parse_url($url)['path']) . substr($l, 1);
-    } else if (substr($l, 0, 1) == '#'){
-      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . parse_url($url)['path'] . $l;
-    } else if (substr($l, 0, 3) == '../'){
-      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . '/' . $l;
-    } else if (substr($l, 0, 11) == 'javascript:'){
-      continue;
-    } else if (substr($l, 0, 5) != 'https' && substr($l, 0, 4) != "http") {
-      $l = parse_url($url)['scheme'] . "://" . parse_url($url)['host'] . '/' . $l;
-    }
-
-    if(!in_array($l, $already_crawled)){
-
-      $already_crawled[] = $l;
-      $crawling[] = $l;
-      echo get_details($l). "\n";
-      // echo $l . "\n";
-
-    }
-
-  }
-
-  // Indefinite Crawl Loop
-  // array_shift($crawling);
-  // foreach ($crawling as $site){
-  //   follow_links($site);
-  // }
-
-}
-
-follow_links($start);
-
-//print_r($already_crawled);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <meta property="og:title" content="Culinary Bot Test" />
+  <meta property="og:description" content="A crawler for up to date feeds pertaining to the culinary world" />
+  <meta property="og:type" content="" />
+  <meta property="og:url" content="" />
+  <meta property="og:image" content="" />
+</head>
+<body>
+  <pre>
+  <?php require_once('bot/bot.php') ?>
+  </pre>
+</body>
+</html><?php $_SERVER['DOCUMENT_ROOT']; ?>
