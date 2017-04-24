29e793c610fcb8e7724860d26dc7abd2faef9387
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-23 23:27:21 -0700

    Used for testing site-feed functions to be placed in parseFunctions.php

--- test.php
+++ test.php
@@ -1,24 +1,21 @@
 <?php
 require "simple_html_dom.php";
 
-$site_name = "Saveur";
-$site_url = "http://www.epicurious.com/";
+$site_name = "Huffington Post | Taste";
+$site_url = "http://www.huffingtonpost.com/section/taste";
 
 function test($site_name, $site_url){
   // Get HTML
   $html = file_get_html($site_url);
 
   // Parse HTML
-  $img_att = "data-lgsrc";
-  $feed_topic = $html->find("div[class=field-page-sections] H3 em", 0)->plaintext;
-  $feed_title = $html->find("div[class=field-page-sections] H3 a", 0)->plaintext;
-  $feed_url = $html->find("div[class=field-page-sections] H3 a", 0)->href;
-  $feed_image = $html->find("div[class=field-image] a img", 0)->img_att;
-
+  $feed_base = $html->find("div[class=a-page__content__splash] section",0);
+  $feed_title = $feed_base->find("H2 a", 0)->plaintext;
+  $feed_url = $feed_base->find("H2 a", 0)->href;
+  $feed_image = $feed_base->find("div[class=splash__image__wrapper] a img", 0)->src;
 
   echo $site_name."\n";
   echo $site_url."\n";
-  echo $feed_topic."\n";
   echo $feed_title."\n";
   echo $feed_url."\n";
   echo $feed_image."\n";
