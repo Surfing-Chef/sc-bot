585cd9ff76d12feff7779fff6c2a760cff377b5a
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-26 14:32:55 -0700

    Tested foodandwine parse settings

--- test.php
+++ test.php
@@ -2,17 +2,19 @@
 require "simple_html_dom.php";
 
 $site_name = "Huffington Post | Taste";
-$site_url = "http://www.huffingtonpost.com/section/taste";
+$site_url = "http://www.foodandwine.com/";
 
 function test($site_name, $site_url){
   // Get HTML
   $html = file_get_html($site_url);
 
   // Parse HTML
-  $feed_base = $html->find("div[class=a-page__content__splash] section",0);
-  $feed_title = $feed_base->find("H2 a", 0)->plaintext;
-  $feed_url = $feed_base->find("H2 a", 0)->href;
-  $feed_image = $feed_base->find("div[class=splash__image__wrapper] a img", 0)->src;
+  $img_att = "srcset";
+  $feed_base = $html->find("article[class=hero-feature]",0);
+  $feed_title = $feed_base->find("div[class=hero-feature__content] span",0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
+  $feed_url = $feed_base->find("a[class=hero-feature__link]",0)->href;
+  $feed_image = $feed_base->find("a[class=hero-feature__link] source",0)->$img_att;
 
   echo $site_name."\n";
   echo $site_url."\n";
