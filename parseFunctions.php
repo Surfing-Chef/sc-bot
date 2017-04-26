5f4a3e3063524b4c9f988c3b64fcc1ce0c8bff06
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-26 00:13:25 -0700

    Replaced any "newline" or multiple spaces with single space.

--- parseFunctions.php
+++ parseFunctions.php
@@ -11,6 +11,7 @@ function epicurious($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("article[class=article-hero-featured-item]",0);
   $feed_title = $feed_base->find("header H4", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("a", 0)->href;
   $feed_url = $site_url.substr($feed_url, 1);
   $feed_image = $html->find("div[class=photo-wrap] source[media=(min-width: 768px)]", 0)->srcset;
@@ -31,6 +32,7 @@ function saveur($site_name, $site_url){
   $img_att = "data-lgsrc";
   $feed_base =$html->find("div[class=field-page-sections]",0);
   $feed_title = $feed_base->find("H3 a", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("H3 a", 0)->href;
   $feed_image = $html->find("div[class=field-image] a img", 0)->$img_att;
 
@@ -48,6 +50,7 @@ function foodandwine($site_name, $site_url){
   $img_att = "data-srcset";
   $feed_base = $html->find("article[class=story-card]",0);
   $feed_title = $feed_base->find("div[class=story-card__content] span", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("a", 0)->href;
   $feed_image = $feed_base->find("div[class=story-card__img-wrap] picture source", 1)->$img_att;
 
@@ -56,7 +59,7 @@ function foodandwine($site_name, $site_url){
 
 }
 
-// Food and Wine
+// Food 52
 function food52($site_name, $site_url){
   // Get HTML
   $html = file_get_html($site_url);
@@ -65,6 +68,7 @@ function food52($site_name, $site_url){
   $img_att = "data-pin-media";
   $feed_base = $html->find("div[class=home-grid]",1);
   $feed_title = $feed_base->find(".home-tile a", 1)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find(".home-tile a", 0)->href;
   $feed_url = $site_url.substr($feed_url, 1);
   $feed_image = $feed_base->find(".home-tile a img", 0)->$img_att;
@@ -82,6 +86,7 @@ function silversurfers($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("div[class=listing_box large]",0);
   $feed_title = $feed_base->find("div[class=abs_box] a",0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("div[class=abs_box] a",0)->href;
   $feed_image = $feed_base->find("div[class=bg_img]", 0)->style;
   $feed_image = substr($feed_image, 21);
@@ -101,6 +106,7 @@ function ucla($site_name, $site_url){
   $img_att = "data-large-file";
   $feed_base = $html->find("#content article",0);
   $feed_title = $feed_base->find("header H1 a",0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("header H1 a",0)->href;
   $feed_image = $feed_base->find("div[class=entry-thumbnail] a img", 0)->$img_att;
 
@@ -118,6 +124,7 @@ function chowhound($site_name, $site_url){
   $img_att = "data-src";
   $feed_base = $html->find("div[class=fr_ftp_promo_box]",0);
   $feed_title = $feed_base->find("div[class=fr_ftp_txt] H2 a", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("div[class=fr_ftp_txt] H2 a", 0)->href;
   $feed_image = $feed_base->find("figure img", 0)->$img_att;
 
@@ -135,6 +142,7 @@ function foodtank($site_name, $site_url){
   $img_att = "data-original";
   $feed_base = $html->find("article figure a", 0);
   $feed_title = $feed_base->title;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->href;
   $feed_image = $html->find("article figure a img", 0)->$img_att;
 
@@ -151,6 +159,7 @@ function thesalt($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("#overflow article[class=has-image]", 0);
   $feed_title = $feed_base->find("div[class=item-info] H2 a", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("div[class=item-info] H2 a", 0)->href;
   $feed_image = $feed_base->find("div[class=imagewrap] a img", 0)->src;
 
@@ -167,6 +176,7 @@ function allrecipes($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("article[class=slider__slide]", 0);
   $feed_title = $feed_base->find("div[class=slider__text] H3", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("a", 0)->href;
   $feed_image = $feed_base->find("a img", 0)->src;
 
@@ -183,6 +193,7 @@ function kitchn($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("div[class=TeaserGrid__item] a",0);
   $feed_title = $feed_base->find("div[class=StandardTeaser__body] span",1)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->href;
   $feed_image = $feed_base->find("div[class=StandardTeaser__picture] img",0)->src;
 
@@ -199,6 +210,7 @@ function taste($site_name, $site_url){
   // Parse HTML
   $feed_base = $html->find("div[class=a-page__content__splash] section",0);
   $feed_title = $feed_base->find("H2 a", 0)->plaintext;
+  $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
   $feed_url = $feed_base->find("H2 a", 0)->href;
   $feed_image = $feed_base->find("div[class=splash__image__wrapper] a img", 0)->src;
 
