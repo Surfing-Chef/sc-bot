c4580b80de6af96f8d1abaec4ce558ba2d248fb9
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-27 17:17:52 -0700

    Tested new randomizer function

--- test.php
+++ test.php
@@ -1,23 +1,43 @@
 <?php
-require "simple_html_dom.php";
+// require "simple_html_dom.php";
+//
+// $site_name = "epicy";
+// $site_url = "http://www.foodandwine.com/";
+//
+// function test($site_name, $site_url){
+//   // Get HTML
+//   $html = file_get_html($site_url);
+//
+//   // Parse HTML
+//   $img_att = "srcset";
+//   $feed_base = $html->find("article[class=hero-feature]",0);
+//   $feed_title = $feed_base->find("div[class=hero-feature__content] span",0)->plaintext;
+//   $feed_title = trim(preg_replace('/\s\s+/', ' ', $feed_title));
+//   $feed_url = $feed_base->find("a[class=hero-feature__link]",0)->href;
+//   $feed_image = $feed_base->find("a[class=hero-feature__link] source",0)->$img_att;
+//   $feed_image = substr(trim($feed_image), 0, -3);
+//
+//   echo $site_name."\n";
+//   echo $site_url."\n";
+//   echo $feed_title."\n";
+//   echo $feed_url."\n";
+//   echo $feed_image."\n";
+// }
+//   test($site_name, $site_url);
 
-$site_name = "Huffington Post | Taste";
-$site_url = "http://www.huffingtonpost.com/section/taste";
+// function to create random numbers
+function randomizer($how_many, $of_how_many){
+  $num_array = array();
 
-function test($site_name, $site_url){
-  // Get HTML
-  $html = file_get_html($site_url);
-
-  // Parse HTML
-  $feed_base = $html->find("div[class=a-page__content__splash] section",0);
-  $feed_title = $feed_base->find("H2 a", 0)->plaintext;
-  $feed_url = $feed_base->find("H2 a", 0)->href;
-  $feed_image = $feed_base->find("div[class=splash__image__wrapper] a img", 0)->src;
-
-  echo $site_name."\n";
-  echo $site_url."\n";
-  echo $feed_title."\n";
-  echo $feed_url."\n";
-  echo $feed_image."\n";
+  while(count($num_array)<$how_many){
+    $num = round(rand(0, $of_how_many-1));
+    if(!in_array($num,$num_array)){
+      $num_array[] = $num;
+    } else {
+      continue;
+    }
+  }
+  print_r($num_array);
 }
-  test($site_name, $site_url);
+
+randomizer(6,12);
